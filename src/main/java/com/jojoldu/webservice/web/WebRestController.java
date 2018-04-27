8f0a4c56e28b2dfccd9ac6e3bd00e0397c65c78b
changdae <shook88@naver.com>
2018-04-27 21:06:25 +0900

    second

--- src/main/java/com/jojoldu/webservice/web/WebRestController.java
+++ src/main/java/com/jojoldu/webservice/web/WebRestController.java
@@ -1,13 +1,34 @@
 package com.jojoldu.webservice.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jojoldu.webservice.domain.posts.PostsRepository;
+import com.jojoldu.webservice.dto.posts.PostsSaveRequestDto;
+import com.jojoldu.webservice.service.PostsService;
+
+import lombok.AllArgsConstructor;
+
+
+@AllArgsConstructor
 @RestController
 public class WebRestController {
+
+	private PostsRepository postsRepository;
+
+	private PostsService postsService;
 
 	@GetMapping("/hello")
 	public String hello() {
 		return "HelloWorld";
 	}
+
+	
+	@PostMapping("/posts")
+	public Long savePosts(@RequestBody PostsSaveRequestDto dto) {
+		return postsService.save(dto);
+	}
+
 }
--- src/test/java/com/jojoldu/webservice/ApplicationTests.java
+++ src/test/java/com/jojoldu/webservice/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.jojoldu.springwebservice;
+package com.jojoldu.webservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringWebserviceApplicationTests {
+public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
